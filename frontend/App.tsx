62de60477cffbac1c6d5a9114c7c50477f23fe92
jaydenpiao <jaydenpiao@gmail.com>
2024-01-20 13:51:49 -0800

    setup basic example of routing

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import Welcome from './components/Welcome';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const App = () => {
+import Welcome from './components/Welcome';
+import Map from './components/Map';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Welcome />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Welcome' component={Welcome} />
+        <Stack.Screen name='Map' component={Map} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 

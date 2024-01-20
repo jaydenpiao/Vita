0764b235a8f420e2f18264a1a0ee12ef8cc4a9d5
junhe-c <cjunhe05@gmail.com>
2024-01-20 14:43:01 -0800

    Implement patient class

--- simulator/main.py
+++ simulator/main.py
@@ -3,6 +3,34 @@ import firebase_admin
 
 from firebase_admin import firestore
 
+class Patient:
+    def __init__ (self, name, age, sex, history):
+        self.name = name
+        self.age = age
+        self.sex = sex
+        self.history = history
+
+    def display_info(self):
+        print(f"Patient Information:")
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Gender: {self.sex}")
+        print ("Medical History: ")
+        if not self.history:
+            print(f"Patient has no history")
+        else:
+            for condition in self.history:
+                print (f" - {condition}")
+
+if __name__ == "__main__":
+    patient1 = Patient(name="John Doe", age=68, sex="Male", history=[])
+
+    patient1.display_info()
+
+    patient2 = Patient(name="Jane Doe", age=72, sex="Female", history=["Hypertension"])
+    patient2.display_info()
+
+"""
 async def my_func(db):
     count = 0
     doc_ref = db.collection("counters").document("counter1")
@@ -13,10 +41,12 @@ async def my_func(db):
         doc_ref.set({"count" : count})
         count = (count + 1) if (count < 9) else 0
 
+
 # Authenticate with firebase
 cred = firebase_admin.credentials.Certificate("./credentials.json")
-firebase_admin.initialize_app(cred)
+app = firebase_admin.initialize_app(cred)
 db = firestore.client()
+
 
 # Create an event loop
 loop = asyncio.get_event_loop()
@@ -32,3 +62,4 @@ except KeyboardInterrupt:
     pass
 finally:
     loop.close()
+"""

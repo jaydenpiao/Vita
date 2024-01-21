0e3dd3f31166ca3166246f178157ca1eeaf9ecd7
Devon Friend <devon.fri.2000@gmail.com>
2024-01-20 16:44:15 -0800

    Implement Simulator class

--- simulator/routines.py
+++ simulator/routines.py
@@ -19,4 +19,18 @@ class PatientRoutine:
         await asyncio.sleep(2)
         print(self.patient)
 
-    
+
+class Simulator:
+    def __init__(self, patients: list):
+        self.patients = patients
+        self.routines = []
+        for patient in patients:
+            self.routines.append(PatientRoutine(patient))
+
+    def start(self):
+        for routine in self.routines:
+            routine.start()
+
+    def stop(self):
+        for routine in self.routines:
+            routine.stop()    

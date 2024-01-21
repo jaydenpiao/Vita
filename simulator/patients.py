f03c6242e8f94151c4dfae7b0f3d5166d715e9bf
junhe-c <cjunhe05@gmail.com>
2024-01-21 05:43:55 -0800

    Added more patients

--- simulator/patients.py
+++ simulator/patients.py
@@ -28,5 +28,31 @@ PATIENT_METADATA = [
             "emergency_contact": Contact(name="Jane Doe", phone="555-555-5555"),
             "sensor_data": SensorData(ecg=[5, 4, 3], location={"lat": 0, "lon": 0})
         }
+    },
+    {
+        "patient_id": "2",
+        "path": ["PATIO", "ROOM 10", "BATH 10", "ROOM 10", "DINING ROOM", "ROOM 10", "PATIO"],
+        "data": {
+            "name": "James Doe",
+            "age": 65,
+            "birthdate": datetime(1958, 6, 23),
+            "room": "300A",
+            "medical_history": "History of hypertension",
+            "emergency_contact": Contact(name="Jane Doe", phone="555-555-5555"),
+            "sensor_data": SensorData(ecg=[1, 2, 3], location={"lat": 0, "lon": 0})
+        }
+    },
+    {
+        "patient_id": "3",
+        "path": ["ROOM 12", "BATH 12", "ROOM 12", "SPA", "SPA BATH", "ROOM 12"],
+        "data": {
+            "name": "Jim Doe",
+            "age": 85,
+            "birthdate": datetime(1938, 9, 2),
+            "room": "400A",
+            "medical_history": "Type I diabetes",
+            "emergency_contact": Contact(name="Jane Doe", phone="555-555-5555"),
+            "sensor_data": SensorData(ecg=[5, 4, 3], location={"lat": 0, "lon": 0})
+        }
     }
 ]
